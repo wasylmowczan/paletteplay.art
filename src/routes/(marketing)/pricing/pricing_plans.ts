a3061743885bbc14db1380104dddf225bf661564
Wasyl Mowczan <wasyl.mowczan@tsh.io>
2024-06-04 16:01:00 +0200

    Remove stripe product id

--- src/routes/(marketing)/pricing/pricing_plans.ts
+++ src/routes/(marketing)/pricing/pricing_plans.ts
@@ -8,7 +8,6 @@ export const pricingPlans = [
     price: "$0",
     priceIntervalName: "per month",
     stripe_price_id: "price_1PNxqPEAQD7sAy8kxu20B27a",
-    stripe_product_id: "prod_QEQhh92dUWIxYh",
     features: [
       "✔️ Create 5 pages for free",
       "✔️ Personal use",
